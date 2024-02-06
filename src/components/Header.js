e99e7bdae15c246809c04bd1238ed2ab2ff71563
Harsh Anand <harsh250600@gmail.com>
2024-02-06 09:48:15 +0530

    react bootstrap installed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { Nav, Navbar, NavDropdown, Image } from "react-bootstrap";
 
-function Header(){
-    return(
+function Header() {
+    return (
         <>
-
+            <Navbar className="navbar-style" expand="lg">
+                <Image src="./images/logo.png" className="nav-logo" roundedCircle />
+                <Navbar.Brand href="#home">Visvesvaraya Technological University</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#">AboutVTU</Nav.Link>
+                        <Nav.Link href="#">Link</Nav.Link>
+                        <Nav.Link href="#">Admission Details</Nav.Link>
+                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#">Another action</NavDropdown.Item>
+                            <NavDropdown.Item href="#">Something</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         </>
     )
 }
+export default Header;
